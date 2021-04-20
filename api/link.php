89c1596a258591a5f6b411e43d89639af8cba44e
focasousa <82666352+focasousa@users.noreply.github.com>
2021-04-20 03:49:39 -0300

    Add files via upload

---
+++ api/link.php
@@ -0,0 +1,6 @@
+<?php 
+// SERVE PARA SALVAR OS LINKS NOS SLOTS DA LICENÇA //
+
+//     [tk] => 33
+//     [slot] => 1
+//     [host] => localhost
---
+++ api/puxar.php
@@ -0,0 +1,12 @@
+<?php
+require_once("../string.php");
+?>
+ 'a' =>1<br>
+ 'b' =>Dev<br>
+ 'c' =>2035-12-15<br>
+ '1' =><?php echo $site_urlvl; ?><br>
+ '2' =><?php echo $site_urlvl; ?><br>
+ '3' =><?php echo $site_urlvl; ?><br>
+
+ 
+
